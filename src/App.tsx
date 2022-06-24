dc95034b33d74d7c9ac542efec905979c76e62be
Yelly Putri <yellyputriw@gmail.com>
2022-06-24 11:10:20 +0700

    :lipstick: add style files

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
+        <div className="header-title">
           <h1 className="title">Typescript CRUD</h1>
           <p className="subtitle">With React and TypeORM</p>
         </div>
